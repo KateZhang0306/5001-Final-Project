52c45e2d10f397d09584adfe03087e4df61a17c6
KateZhang0306 <kaijia.zhang@duke.edu>
2024-12-03 07:43:34 -0500

    change names

---
+++ log_tracker.py
@@ -0,0 +1,9 @@
+
+
+import json
+from datetime import date
+
+empty_json = {} 
+empty_json_string = json.dumps(empty_json) 
+
+LOG_FILE = "empty_json.json"
---
+++ main_tracker.py
@@ -0,0 +1,7 @@
+
+
+from food_entry import load_data, search_food, add_food
+
+def main():
+    food_database = load_data()
+    
